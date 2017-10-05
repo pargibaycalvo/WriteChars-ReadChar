65ef6bbe5666a75a6ef9cdf58327fd76c07260cb
oracle <oracle@localhost>
2017-10-05 10:51:08 +0200

    writecharsreadchar

---
+++ src/primitivereadchar/PrimitiveReadChar.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primitivereadchar;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author oracle
+ */
+public class PrimitiveReadChar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        texto txt = new texto();
+        txt.carpeta();
+        txt.archivo();
+        txt.escritura("Esta e unha cadea \n ");
+        txt.lectura();
+
+    }
+    
+}
